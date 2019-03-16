1e0af1473701c267ff5c86aa2920393a77944985
Dennis Kim <shinsa@Denniss-MacBook-Pro.local>
2019-03-15 18:59:39 -1000

    contracts collection finished in 37 minutes

--- app/imports/ui/components/ContactAdmin.jsx
+++ app/imports/ui/components/ContactAdmin.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
 
 /** Renders a single row in the List Stuff table. See pages/ListStuff.jsx. */
-class Contact extends React.Component {
+class ContactAdmin extends React.Component {
   render() {
     return (
         <Card centered>
@@ -16,15 +16,18 @@ class Contact extends React.Component {
             </Card.Meta>
             <Card.Description>{this.props.contact.description}</Card.Description>
           </Card.Content>
+          <Card.Content extra>
+            {this.props.contact.owner}
+          </Card.Content>
         </Card>
     );
   }
 }
 
 /** Require a document to be passed to this component. */
-Contact.propTypes = {
+ContactAdmin.propTypes = {
   contact: PropTypes.object.isRequired,
 };
 
 /** Wrap this component in withRouter since we use the <Link> React Router element. */
-export default withRouter(Contact);
+export default withRouter(ContactAdmin);
